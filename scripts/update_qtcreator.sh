2a9bc876a1730b3a98d6d6aac960fce6e2542087
Hugo Costelha <hugo.costelha@gmail.com>
2015-02-23 17:27:49 +0000

    Minor change to the Qt updater

--- scripts/update_qtcreator.sh
+++ scripts/update_qtcreator.sh
@@ -3,8 +3,8 @@
 # We need to update qtcreator, since the version included in Kubuntu has some problems
 echo "--> Downloading updated Qt"
 wget http://download.qt.io/official_releases/qt/5.4/5.4.0/qt-opensource-linux-x64-5.4.0.run
-echo "--> Removoing old QtCreator"
-sudo apt-get remove qtcreator qtcreator-doc qtcreator-plugin-remotelinux qtcreator-plugin-valgrind qtcreator-plugin-qnx qtcreator-plugin-cmake
+echo "--> Removing old QtCreator"
+sudo apt-get -y remove qtcreator qtcreator-doc qtcreator-plugin-remotelinux qtcreator-plugin-valgrind qtcreator-plugin-qnx qtcreator-plugin-cmake
 chmod +x qt-opensource-linux-x64-5.4.0.run
 echo "--> Updating qt. Accept all default options in the window"
 sudo ./qt-opensource-linux-x64-5.4.0.run
