c28da236165a3b8a733b45f3d9f938dd306601c4
Hugo Costelha <hugo.costelha@ipleiria.pt>
2019-02-22 00:56:29 +0000

    Use $HOME instead of ~

--- scripts/install_RA.sh
+++ scripts/install_RA.sh
@@ -46,7 +46,7 @@ rosdep update
 # Set up variables and Clone our gir repository, if not already cloned
 if [ ! -d "$HOME/ros" ]; then
   echo "Setting ROS environment variables..." 
-  echo "" >> ~/.bashrc
+  echo "" >> $HOME/.bashrc
   echo "# ROS Environment variables" >> ~/.bashrc
   echo ". /opt/ros/melodic/setup.bash" >> ~/.bashrc
   echo ". $HOME/ros/devel/setup.bash" >> ~/.bashrc
@@ -58,7 +58,7 @@ fi
 source "/opt/ros/melodic/setup.bash"
 
 ROS_PYTHON_VERSION=3
-catkin_make -C ~/ros
+catkin_make -C $HOME/ros
 source "$HOME/ros/devel/setup.bash"
 
 # Qt
