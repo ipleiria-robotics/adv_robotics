1e1c3a8fda74cfdf18e0cb0b985c7f714a5fc4f9
Hugo Costelha <hugo.costelha@ipleiria.pt>
2022-03-25 11:58:52 +0000

    Adding TW04-Trilateration.

---
+++ src/tw04/tw04/utils.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2020, Hugo Costelha
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice,
+#     this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions and the following disclaimer in the documentation
+#     and/or other materials provided with the distribution.
+# * Neither the name of the Player Project nor the names of its contributors
+#     may be used to endorse or promote products derived from this software
+#     without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE 4 ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Revision $Id$
+
+# Library packages needed
+from math import atan2, cos, sin
+import sys
+import numpy as np
+
+# ROS
+from geometry_msgs.msg import Quaternion
+
+
+
+def clipValue(value: float, min: float, max: float) -> float:
+    '''Clip a given value to the interval [min, max]'''
+    if value > max:
+        return max
+    elif value < min:
+        return min
+    else:
+        return value
+
+
+def quaternionToYaw(q):
+    '''Returns the yaw in radians of a quaternion.
+    Reimplements part of euler_from_quaternion from the tf package because tf
+    doesn't play well in Python 3.
+    '''
+    t0 = 2.0 * (q.w * q.z + q.x * q.y)
+    t1 = 1.0 - 2.0 * (q.y**2 + q.z**2)
+    return atan2(t0, t1)
+
+
+def rpyToQuaternion(roll, pitch, yaw) -> np.matrix:
+    '''Returns the quaternian corresponding to a yaw rotation. Reimplements
+    part of the tf package because tf doesn't play well in Python 3.
+    '''
+    halfYaw = yaw * 0.5
+    halfPitch = pitch * 0.5
+    halfRoll = roll * 0.5
+    cosYaw = cos(halfYaw)
+    sinYaw = sin(halfYaw)
+    cosPitch = cos(halfPitch)
+    sinPitch = sin(halfPitch)
+    cosRoll = cos(halfRoll)
+    sinRoll = sin(halfRoll)
+    return Quaternion(
+        x=sinRoll * cosPitch * cosYaw - cosRoll * sinPitch * sinYaw,
+        y=cosRoll * sinPitch * cosYaw + sinRoll * cosPitch * sinYaw,
+        z=cosRoll * cosPitch * sinYaw - sinRoll * sinPitch * cosYaw,
+        w=cosRoll * cosPitch * cosYaw + sinRoll * sinPitch * sinYaw)
+
+###############################################################################
+# Terminal writing related functions
+###############################################################################
+
+
+def printxy(x, y, text):
+    '''Print in a specified screen position'''
+    sys.stdout.write('\x1b[%d;%df%s' % (x, y, text))
+
+
+def clearTerminal():
+    '''Clear terminarl
+       http://ascii-table.com/ansi-escape-sequences.php'''
+    sys.stdout.write('\x1b[2J')
+
+
+def clearLine():
+    '''Clear current terminar line
+       http://ascii-table.com/ansi-escape-sequences.php'''
+    sys.stdout.write('\x1b[K')
