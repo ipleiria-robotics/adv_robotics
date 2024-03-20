1a29eb832511f476ad49594eb1f08b54d591a01a
Hugo Costelha <hugo.costelha@ipleiria.pt>
2024-03-20 19:50:49 +0000

    Work continuous to use existing NAV2 implementation to make the implementations easier. TW07 is now working with AMCL

--- src/tw07/launch/tw07_launch.py
+++ src/tw07/launch/tw07_launch.py
@@ -1,67 +1,101 @@
 from simple_launch import SimpleLauncher
-import yaml
+from launch_ros.descriptions import ParameterFile
+from nav2_common.launch import RewrittenYaml
+from launch.substitutions import TextSubstitution
 import os
 
-lifecycle_nodes = ['map_server']
 
-sl = SimpleLauncher(use_sim_time=True)
+def generate_launch_description():
+    lifecycle_nodes = ['map_server', 'amcl']
 
-# Declare arguments
-sl.declare_arg('params_file',
-               sl.find('tw07', 'params.yaml', 'config'),
-               description='(Optional) Complete path to the parameters file')
-sl.declare_arg('start_stage', True,
-               description='If True, the Stage simulator is started')
+    sl = SimpleLauncher(use_sim_time=True)
 
+    # Declare arguments
+    sl.declare_arg(
+        'namespace',
+        'robot_0',
+        description='Top-level robot namespace.')
+    namespace = sl.arg('namespace')
+    sl.declare_arg(
+        'params_file',
+        sl.find('tw07', 'params.yaml', 'config'),
+        description='(Optional) Complete path to the parameters file')
+    # Get the YAML configuration file
+    params = sl.arg('params_file')
+    #configured_params = params
+    configured_params = ParameterFile(
+        RewrittenYaml(
+            source_file=params,
+            root_key=namespace,
+            param_rewrites={'base_frame_id': sl.arg(namespace)+'/base_footprint',
+                            'odom_frame_id': sl.arg(namespace)+'/odom'},
+            convert_types=True,
+        ),
+        allow_substs=True,
+    )
 
-def launch_opaque_setup():
+    # Stage simulator
+    sl.declare_arg('start_stage', True,
+                   description='If True, the Stage simulator is started')
+    sl.node(package='stage_ros',
+            executable='stageros',
+            name='stageros',
+            output='screen',
+            arguments=sl.find('worlds', 'map_laser_with_landmarks2.world',
+                              'stage_worlds'))
 
-    with sl.group(if_arg='start_stage'):
-        # Get the YAML configuration file
-        params = sl.arg('params_file')
-        with open(params, 'r') as file:
-            params_raw = yaml.safe_load(file)
+    # Map server
+    sl.node(package='nav2_map_server',
+            executable='map_server',
+            name='map_server',
+            namespace=namespace,
+            output='screen',
+            emulate_tty=True,  # https://github.com/ros2/launch/issues/188
+            parameters=[
+                {'yaml_filename': sl.find('tw07', 'map.yaml', 'config')}])
 
-        # Get the world file from the parameters file
-        world_pkg = params_raw['stage']['world_package']
-        world_path = params_raw['stage']['world_file']
-        world_dir, world_filename = os.path.split(world_path)
+    # RViz
+    sl.rviz(sl.find('tw07', 'config.rviz', 'config'),
+            warnings=True)
 
-        # Stage simulator
-        sl.node('stage_ros', 'stageros',
-                arguments=sl.find(world_pkg, world_filename, world_dir))
+    # Ground truth republisher
+    sl.node(package='tw07',
+            executable='ground_truth_republisher',
+            name='tw07_ground_truth_republisher',
+            namespace=namespace,
+            output='screen')
 
-        # Get the map file from the parameters file
-        map_pkg = params_raw['map_server']['map_package']
-        map_path = params_raw['map_server']['map_filename']
-        map_dir, map_filename = os.path.split(map_path)
+    # NAV2-based particle filter
+    sl.node(package='nav2_amcl',
+            executable='amcl',
+            name='amcl',
+            namespace=namespace,
+            output='screen',
+            parameters=[configured_params])
 
-        # Map server
-        sl.node('nav2_map_server',
-                'map_server',
-                name='map_server',
+    # Path navigation node (from TW03)
+    sl.declare_arg('run-navigation', True,
+                   description='If True, run the navigation node')
+    with sl.group(if_arg='run-navigation'):
+        sl.node(package='tw03',
+                executable='navigation',
+                name='navigation',
+                namespace=namespace,
                 output='screen',
-                emulate_tty=True,  # https://github.com/ros2/launch/issues/188
-                parameters=[
-                    {'yaml_filename': sl.find(map_pkg, map_filename, map_dir)},
-                    params_raw['map_server']['ros__parameters']])
-        
-        # Get the rviz confiuration file
-        rviz_cfg_pkg = params_raw['rviz']['cfg_package']
-        rviz_cfg_path = params_raw['rviz']['cfg_filename']
-        rviz_cfg_dir, rviz_cfg_filename = os.path.split(rviz_cfg_path)
+                emulate_tty=True,
+                parameters=[configured_params])
 
-        # Rviz server
-        sl.node('rviz2',
-                'rviz2',
-                name='rviz',
-                output='screen',
-                emulate_tty=True,  # https://github.com/ros2/launch/issues/188
-                parameters=[
-                        {'use_sim_time': use_sim_time}],
-                arguments=['-d', sl.find(rviz_cfg_pkg, rviz_cfg, rviz_cfg_dir)])
+    # Start lifecycle node manager
+    node_names = [sl.py_eval(sl.arg(namespace) + '/') + node_name for node_name in lifecycle_nodes]
+    #node_names = [os.path.join(sl.arg(namespace), node_name) for node_name in lifecycle_nodes]
+    
+    sl.node(package='nav2_lifecycle_manager',
+            executable='lifecycle_manager',
+            name='lifecycle_manager',
+            namespace=namespace,
+            output='screen',
+            emulate_tty=True,  # https://github.com/ros2/launch/issues/188
+            parameters=[{'autostart': True},
+                        {'node_names': lifecycle_nodes}])
+
     return sl.launch_description()
-
-
-generate_launch_description = \
-    sl.launch_description(opaque_function=launch_opaque_setup)
