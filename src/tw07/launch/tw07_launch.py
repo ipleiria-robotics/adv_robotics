3243f1a9c1ab2227718896d32e710585d9c8de1f
Hugo Costelha <hugo.costelha@ipleiria.pt>
2024-04-26 11:47:08 +0100

    Update on using nav2 for TW07.

--- src/tw07/launch/tw07_launch.py
+++ src/tw07/launch/tw07_launch.py
@@ -1,44 +1,46 @@
 from simple_launch import SimpleLauncher
 from launch_ros.descriptions import ParameterFile
 from nav2_common.launch import RewrittenYaml
-from launch.substitutions import TextSubstitution
-import os
 
 
 def generate_launch_description():
-    lifecycle_nodes = ['map_server', 'amcl']
+    lifecycle_nodes = ['map_server', 'amcl', 'planner_server']
 
     sl = SimpleLauncher(use_sim_time=True)
 
     # Declare arguments
     sl.declare_arg('namespace', 'robot_0',
-        description='Top-level robot namespace.')
+                   description='Top-level robot namespace.')
     namespace = sl.arg('namespace')
-    sl.declare_arg('params_file', sl.find('tw07', 'params.yaml', 'config'),
+    sl.declare_arg(
+        'params_file', sl.find('tw07', 'params.yaml', 'config'),
         description='(Optional) Complete path to the parameters file')
     # Get the YAML configuration file
     params = sl.arg('params_file')
-    #configured_params = params
+    # configured_params = params
     configured_params = ParameterFile(
         RewrittenYaml(
             source_file=params,
             root_key=namespace,
-            param_rewrites={'base_frame_id': sl.arg(namespace)+'/base_footprint',
-                            'odom_frame_id': sl.arg(namespace)+'/odom'},
+            param_rewrites={
+                'base_frame_id': sl.arg(namespace)+'/base_footprint',
+                'odom_frame_id': sl.arg(namespace)+'/odom',
+                'robot_base_frame': sl.arg(namespace)+'/base_footprint'},
             convert_types=True,
         ),
         allow_substs=True,
     )
 
     # Stage simulator
-    sl.declare_arg('run_stage', True,
+    sl.declare_arg('run-stage', True,
                    description='If True, the Stage simulator is started')
-    sl.node(package='stage_ros',
-            executable='stageros',
-            name='stageros',
-            output='screen',
-            arguments=sl.find('worlds', 'map_laser_with_landmarks2.world',
-                              'stage_worlds'))
+    with sl.group(if_arg='run-stage'):
+        sl.node(package='stage_ros',
+                executable='stageros',
+                name='stageros',
+                output='screen',
+                arguments=sl.find('worlds', 'map_laser_with_landmarks2.world',
+                                  'stage_worlds'))
 
     # Map server
     sl.node(package='nav2_map_server',
@@ -51,8 +53,10 @@ def generate_launch_description():
                 {'yaml_filename': sl.find('tw07', 'map.yaml', 'config')}])
 
     # RViz
-    sl.rviz(sl.find('tw07', 'config.rviz', 'config'),
-            warnings=True)
+    sl.declare_arg('run-rviz', True,
+                   description='If True, RViz is started')
+    with sl.group(if_arg='run-rviz'):
+        sl.rviz(sl.find('tw07', 'config.rviz', 'config'), warnings=True)
 
     # Ground truth republisher
     sl.node(package='tw07',
@@ -69,17 +73,25 @@ def generate_launch_description():
             output='screen',
             parameters=[configured_params])
 
+    # NAV2-based path planner
+    sl.node(package='nav2_planner',
+            executable='planner_server',
+            name='planner_server',
+            namespace=namespace,
+            output='screen',
+            parameters=[configured_params])
+
     # Path navigation node (from TW03)
-    sl.declare_arg('run-navigation', True,
-                   description='If True, run the navigation node')
-    with sl.group(if_arg='run-navigation'):
-        sl.node(package='tw03',
-                executable='navigation',
-                name='navigation',
-                namespace=namespace,
-                output='screen',
-                emulate_tty=True,
-                parameters=[configured_params])
+    #sl.declare_arg('run-navigation', True,
+    #               description='If True, run the navigation node')
+    #with sl.group(if_arg='run-navigation'):
+    #    sl.node(package='tw03',
+    #            executable='navigation',
+    #            name='navigation',
+    #            namespace=namespace,
+    #            output='screen',
+    #            emulate_tty=True,
+    #            parameters=[configured_params])
 
     # Start lifecycle node manager
     sl.node(package='nav2_lifecycle_manager',
